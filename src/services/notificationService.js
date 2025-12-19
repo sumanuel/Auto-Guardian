ce0f57a6382c78527bf513138c29cf45bc46b985
Jesus Prada <jesusprada177@gmail.com>
2025-12-19 10:09:09 -0400

    feat: update notification handler to show banner and list for improved user experience

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -4,7 +4,8 @@ import { Platform } from "react-native";
 // Configurar comportamiento de notificaciones
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
-    shouldShowAlert: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
     shouldPlaySound: true,
     shouldSetBadge: true,
   }),
